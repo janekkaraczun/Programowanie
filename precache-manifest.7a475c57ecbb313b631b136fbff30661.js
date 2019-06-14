4389dd41179c86cafd926bed04766ea2b71ceee8
Jan Karaczun <janek.karaczun@yahoo.com>
2019-06-14 13:40:17 +0200

    Updates

---
+++ precache-manifest.7a475c57ecbb313b631b136fbff30661.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d854da28d20c247e4d44843528928689",
+    "url": "/My-resume/index.html"
+  },
+  {
+    "revision": "1fc5c34a28123a9a24fd",
+    "url": "/My-resume/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "eec7cca1f26da6015303",
+    "url": "/My-resume/static/css/main.660e964e.chunk.css"
+  },
+  {
+    "revision": "1fc5c34a28123a9a24fd",
+    "url": "/My-resume/static/js/2.a3e552ee.chunk.js"
+  },
+  {
+    "revision": "eec7cca1f26da6015303",
+    "url": "/My-resume/static/js/main.78e77ab6.chunk.js"
+  },
+  {
+    "revision": "6632d84faff90baef753",
+    "url": "/My-resume/static/js/runtime~main.4a55eaa0.js"
+  }
+]);
