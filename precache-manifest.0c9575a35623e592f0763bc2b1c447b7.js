d639a7e25a34d11e0f3be816689e4aa4cdcf09d3
Jan Karaczun <janek.karaczun@yahoo.com>
2019-06-25 15:27:20 +0200

    Updates

--- precache-manifest.0c9575a35623e592f0763bc2b1c447b7.js
+++ precache-manifest.0c9575a35623e592f0763bc2b1c447b7.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f09a1f4fa78475ab1b48e461867b7bfc",
+    "revision": "8a2d784b7f2ada06daee4474d5890b06",
     "url": "/My-resume/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/My-resume/static/css/2.60a6b8e0.chunk.css"
   },
   {
-    "revision": "4a24d708b6e5f3401fae",
-    "url": "/My-resume/static/css/main.92927bc6.chunk.css"
+    "revision": "a75465d7adc64a1bf2e8",
+    "url": "/My-resume/static/css/main.f64fdb00.chunk.css"
   },
   {
     "revision": "1fc5c34a28123a9a24fd",
     "url": "/My-resume/static/js/2.a3e552ee.chunk.js"
   },
   {
-    "revision": "4a24d708b6e5f3401fae",
-    "url": "/My-resume/static/js/main.9abdc3d4.chunk.js"
+    "revision": "a75465d7adc64a1bf2e8",
+    "url": "/My-resume/static/js/main.d3c0fd97.chunk.js"
   },
   {
     "revision": "6632d84faff90baef753",
